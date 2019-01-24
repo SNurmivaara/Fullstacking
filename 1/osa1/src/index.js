7422804f88e5f57fdee502fe6973883d1e4eb03f
Sami Nurmivaara <sami.nurmivaara@helsinki.fi>
2019-01-24 14:05:06 +0200

    Added directory for part 1 exercises and did the practice parts in the material

---
+++ 1/osa1/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      Greeting app created by <a href="t.me/indeksi">Indeksi</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name={nimi} age={ika}/>
+      <Footer />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
