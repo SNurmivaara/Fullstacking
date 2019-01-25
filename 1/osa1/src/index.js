68ddcd1f979e1537d39c0b01edaaff5539f57b9d
Sami Nurmivaara <sami.nurmivaara@helsinki.fi>
2019-01-25 09:24:08 +0200

    Part 1.d training done

--- 1/osa1/src/index.js
+++ 1/osa1/src/index.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-/* const Hello = ({ name, age }) => {
+/* 
+
+1.a
+
+const Hello = ({ name, age }) => {
   const bornYear = () => new Date().getFullYear() - age
 
   return (
@@ -26,6 +30,11 @@ const App = () => {
     </div>
   )
 } */
+
+
+/*
+
+1.b-c
 
 const Display = ({ counter }) => <div>{counter}</div>
 
@@ -53,6 +62,53 @@ const App = (props) => {
     </div>
   )
 }
+
+*/
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        sovellusta käytetään nappeja painelemalla
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      näppäilyhistoria: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const[allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
+  }
+
+  return (
+    <div>
+      <div>
+        {left}
+        <button onClick={handleLeftClick}>vasen</button>
+        <button onClick={handleRightClick}>oikea</button>
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    </div>
+  )
+}
+
 
 ReactDOM.render(
   <App />,
