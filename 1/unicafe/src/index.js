d136d019780fe7f62bef89792dccb33638ba48f0
Sami Nurmivaara <sami.nurmivaara@helsinki.fi>
2019-01-26 12:38:00 +0200

    Everything until part 1.d.11 done

--- 1/unicafe/src/index.js
+++ 1/unicafe/src/index.js
@@ -1,21 +1,41 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const Title = props => {
-  return (
-    <h1>{props.title}</h1>
-  )
-}
+const Title = props => (
+  <h1>{props.title}</h1>
+)
+
+const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+)
+
+const Statistic = ({ name, value, suffix}) => (
+  <td>{name} {value} {suffix}</td>
+)
 
 const Statistics = ({good, neutral, bad}) => {
+    if (good + neutral + bad === 0) {
+      return (
+        <div>
+          <p>Ei yhtään palautetta annettu</p>
+        </div>
+      )
+    }
+
     return (
       <div>
-        <p>Hyvä: {good}</p>
-        <p>Neutraali: {neutral}</p>
-        <p>Huono: {bad}</p>
-        <p>Yhteensä: {good+neutral+bad}</p>
-        <p>Keskiarvo: {(good-bad)/(good+neutral+bad)}</p>
-        <p>Positiivisia: {good/(neutral+bad+good)}</p>
+        <table>
+          <tbody>
+            <tr><Statistic name='Hyvä' value={good} /></tr>
+            <tr><Statistic name='Neutraali' value={neutral}/></tr>
+            <tr><Statistic name='Huono' value={bad} /></tr>
+            <tr><Statistic name='Yhteensä' value={good+neutral+bad} /></tr>
+            <tr><Statistic name='Keskiarvo' value={(good-bad)/(good+neutral+bad)} /></tr>
+            <tr><Statistic name='Positiivisia' value={(good/(neutral+bad+good))*100} suffix='%'/></tr>
+          </tbody>
+        </table>
       </div>
     )
 }
@@ -34,9 +54,9 @@ const App = () => {
   return (
     <div>
       <Title title='Anna palautetta'/>
-        <button onClick={handleGoodClick}>hyvä</button>
-        <button onClick={handleNeutralClick}>neutraali</button>
-        <button onClick={handleBadClick}>huono</button>
+        <Button handleClick={handleGoodClick} text='Hyvä' />
+        <Button handleClick={handleNeutralClick} text='Neutraali' />
+        <Button handleClick={handleBadClick} text='Huono' />
       <Title title='Statistiikka' />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
