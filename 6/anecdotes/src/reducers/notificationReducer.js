07b50a97ae64e836bcefef0b8a4415232a57a613
Sami Nurmivaara <sami.nurmivaara@helsinki.fi>
2019-03-04 17:34:10 +0200

    6.21. is wip, can't make it to deadline

--- 6/anecdotes/src/reducers/notificationReducer.js
+++ 6/anecdotes/src/reducers/notificationReducer.js
@@ -11,6 +11,16 @@ export const resetNotification = () => {
   }
 }
 
+export const setNotification = ({ text, timer }) => {
+  return async dispatch => {
+    dispatch({
+      type: "SET_NOTIFICATION_TIMED",
+      text: text,
+      timer: timer
+    })
+  }
+}
+
 const notificationReducer = (state = null, action) => {
   switch (action.type) {
     case "SET_NOTIFICATION":
@@ -18,6 +28,12 @@ const notificationReducer = (state = null, action) => {
       return state
     case "RESET":
       return null
+    case "SET:NOTIFICATION_TIMED":
+      state = action.text
+      setTimeout(() => {
+        state = null
+      }, action.timer*100)
+      return state
     default:
       return state
   }
