dc2653ec7c032c03623094e3661733e9758537cb
Sami Nurmivaara <sami.nurmivaara@helsinki.fi>
2019-03-04 16:21:33 +0200

    ex. 6.17. done

--- 6/anecdotes/src/components/AnecdoteForm.js
+++ 6/anecdotes/src/components/AnecdoteForm.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { notificationChange, resetNotification } from '../reducers/notificationReducer'
+import anecdoteService from "../services/anecdotes"
 
 const AnecdoteForm = (props) => {
-  const addAnecdote = (event) => {
+
+  const addAnecdote = async (event) => {
     event.preventDefault()
-    props.createAnecdote(event.target.anecdote.value)
+    const content = event.target.anecdote.value
+    const newAnecdote = await anecdoteService.createNew(content)
+    props.createAnecdote(newAnecdote.content)
 
     props.notificationChange("New anecdote \"" + event.target.anecdote.value + "\" added!")
 
